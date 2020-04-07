9be0d569d91be3f0511fa63a229f8e8da8e98a19
KonstantinModin <konstantin.v.modin@gmail.com>
2020-04-07 15:45:53 +0200

    updates

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import './app-header.css';
+import React from "react";
+import "./app-header.css";
 
-const AppHeader = ({toDo, done}) => {
-  return (
-    <div className="app-header d-flex">
-      <h1>Todo List</h1>
-      <h2>{toDo} more to do, {done} done</h2>
-    </div>
-  );
+const AppHeader = ({ toDo, done }) => {
+    return (
+        <div className="app-header d-flex">
+            <h1>Todo List</h1>
+            <h2>
+                {toDo} more to do, {done} done
+            </h2>
+        </div>
+    );
 };
 
 export default AppHeader;
--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -1,44 +1,43 @@
-import React, { Component} from 'react';
-import './item-add-form.css';
+import React, { Component } from "react";
+import "./item-add-form.css";
 
 export default class ItemAddForm extends Component {
-
     state = {
-        label: ''
+        label: "",
     };
-
 
     onLabelChange = (e) => {
         this.setState({
-            label: e.target.value
-        });        
+            label: e.target.value,
+        });
     };
 
     onSubmit = (e) => {
         e.preventDefault();
         this.props.onItemAdded(this.state.label);
         this.setState({
-            label: ''
+            label: "",
         });
-
     };
 
     render() {
         return (
-            <form className="item-add-form d-flex"
-                   onSubmit={this.onSubmit}>
-                <input type="text"
-                        className="form-control"
-                        onChange={this.onLabelChange}
-                        placeholder="What needs to be done"
-                        value={this.state.label} />
-                <button 
-                    type="button" 
+            <form className="item-add-form d-flex" onSubmit={this.onSubmit}>
+                <input
+                    type="text"
+                    className="form-control"
+                    onChange={this.onLabelChange}
+                    placeholder="What needs to be done"
+                    value={this.state.label}
+                />
+                <button
+                    type="button"
                     className="btn btn-outline-secondary"
-                    onClick={ this.onSubmit }> 
+                    onClick={this.onSubmit}
+                >
                     Add Item
                 </button>
             </form>
-        )
+        );
     }
-};
+}
